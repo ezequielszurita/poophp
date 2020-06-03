c1a5ff94fe5d10179253cadd2f8d04a7ba8d6288
EzeZurita <ezequielszurita@gmail.com>
2020-06-03 00:16:55 -0300

    se añadio vista v1.0

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<?php
+
+    class Persona{
+
+    //Atributos
+    public $nombre = "Pedro";
+
+    //Metodos
+    public function hablar($mensaje){
+        echo $mensaje;
+
+    }
+}
+
+    $persona = new Persona();
+    //echo $persona -> nombre;
+    $persona->hablar("Saludos desde Codigo Facilito");
+
+
+?>
