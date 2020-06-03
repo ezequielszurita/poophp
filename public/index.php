608b402a028b27b518739666250d2277ddc09bdb
EzeZurita <ezequielszurita@gmail.com>
2020-06-03 18:00:26 -0300

    5.- PHP Orientado a Objetos - Modificadores de acceso

--- public/index.php
+++ public/index.php
@@ -1,48 +1,31 @@
 <?php
+    class Facebook{
 
-    class Loteria
-    {
-        public $numero;
-        public $intentos;
-        public $resultado = false;
+        public $nombre;
+        public $edad;
+        private $pass;
 
-        public function __construct($numero, $intentos)
-        {
-            $this->numero = $numero;
-            $this->intentos = $intentos;
+        public function __construct($nombre, $edad, $pass){
+            $this->nombre = $nombre;
+            $this->edad = $edad;
+            $this->pass = $pass;
         }
 
-        public function sortear()
-        {
-            $minimo = $this->numero / 2;
-            $maximo = $this->numero * 2;
-            for ($i = 0; $i < $this->intentos; $i++) {
-                $int = rand($minimo, $maximo);
-                $this->intentos($int);
-            }
+        public function verInformacion(){
+            echo "Nombre: " . $this->nombre ."<br>";
+            echo "Edad: " . $this->edad ."<br>";
+            $this->cambiarPass("123456");
+            echo "Clave: " . $this->pass ."<br>";
+
         }
-
-        public function intentos($int)
-        {
-            if ($int == $this->numero) {
-                echo "<b>" . $int . " == " . $this->numero . "</b><br>";
-                $this->resultado=true;
-            } else {
-                echo $int . " != " . $this->numero . "<br>";
-            }
-        }
-
-
-        public function __destruct()
-        {
-            if ($this->resultado) {
-                echo "Felicidades, has acertado en " . $this->intentos . " intentos";
-            } else {
-                echo "Que lastima, has perdido en " . $this->intentos . " intentos";
-            }
+        private function cambiarPass($pass){
+            $this->pass = $pass;
         }
     }
 
-        $loteria = new Loteria(10,5);
-        $loteria->sortear();
+    $facebook = new Facebook("Ezequiel",20,"NadieSabe");
+    //echo $facebook->pass;
+    //$facebook->cambiarPass("123456");
+    $facebook->verInformacion();
+
 ?>
