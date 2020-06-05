9c940cf1bef9412c5689e0117d8dc7fab4000b15
EzeZurita <ezequielszurita@gmail.com>
2020-06-05 01:38:16 -0300

    8.- PHP Orientado a Objetos - Interfaces

--- public/index.php
+++ public/index.php
@@ -1,22 +1,76 @@
 <?php
 
-    class Pagina{
-        //Atributos
-        public $nombre = "Codigo Facilito";
-        public static $url = "www.codigofacilito.com";
-
-        //Metodos
-        public function bienvenida(){
-            echo "Bienvenidos a <b> ". $this->nombre . "<br></bt></b> La pagina es <b>" . Pagina::$url ."</b>";
-        }
-        public static function bienvenida2(){
-            echo "Bienvenidos a " . Pagina::$url;
-        }
+    interface Auto{
+        public function encender();//siempre deben ser publicos
+        public function apagar();
+            }
+    interface gasolina extends Auto{
+        public function vaciarTanque();
+        public function llenarTanque($cant);
     }
 
-    class Web extends Pagina{
+    class Deportivo implements gasolina {
 
+            private $estado = false;
+            private $tanque = 0;
+
+            public function estado(){
+                if ($this->estado){
+                    print "El auto esta encendido y tiene <br>" . $this->tanque . " de Litros en el tanque";}
+                else{
+                    print "El auto esta apagado <br>";
+            }}
+
+            public function encender()
+            {
+                if ($this->estado) {
+                    print "No puedes encender el auto 2 veces <br>";
+                }
+                else{
+                        if ($this->tanque <= -0) {
+                            print "Usted no puede encender el auto por que el tanque esta vacio <br>";
+                        } else {
+                            print "Auto encendido <br>";
+                            $this->estado = true;
+                        }
+                    }
+                }
+
+            public function apagar(){
+                if ($this->estado){
+                    print "Auto Apagado <br>";
+                    $this->estado = false;}
+                else{
+                    print "El auto ya esta apagado <br>";}
+            }
+
+            public function vaciarTanque(){
+                $this->tanque = 0;
+            }
+
+            public function llenarTanque($cant){
+                $this->tanque = $cant;
+
+            }
+
+            public function usar($km){
+                if($this->estado){
+                    $reducir = $km /3;
+                    $this->tanque = $this->tanque - $reducir;
+                    if($this->tanque <= 0){
+                        $this->estado = false;
+                    }
+                }else{
+                    print "El auto esta apagado y no se puede usar";
+                }
+
+            }
     }
 
-    Web::bienvenida2();
+    $obj = new Deportivo();
+    $obj->llenarTanque(100);
+    $obj->encender();
+    $obj->usar(350);
+    $obj->estado();
+
 ?>
